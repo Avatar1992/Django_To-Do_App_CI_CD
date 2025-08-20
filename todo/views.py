7751a48c66ae91796c3630a8b5e95b75a58c3a3b
Avatar Patle <157494778+Avatar1992@users.noreply.github.com>
2025-08-21 02:51:43 +0530

    Update views.py

--- todo/views.py
+++ todo/views.py
@@ -1,25 +1,10 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import redirect
 from .models import Task
-from .forms import TaskForm
 
-def index(request):
-    tasks = Task.objects.all()
-    form = TaskForm()
+def add_task(request):
     if request.method == "POST":
-        form = TaskForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("index")
-    return render(request, "todo/index.html", {"tasks": tasks, "form": form})
-
-def delete_task(request, pk):
-    task = get_object_or_404(Task, id=pk)
-    task.delete()
-    return redirect("index")
-
-def toggle_task(request, pk):
-    task = get_object_or_404(Task, id=pk)
-    task.completed = not task.completed
-    task.save()
+        title = request.POST.get("title")
+        if title:
+            Task.objects.create(title=title)
     return redirect("index")
 
