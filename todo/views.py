8734505bfdea36297fdfe1690803235ccccd86c8
Avatar Patle <157494778+Avatar1992@users.noreply.github.com>
2025-08-21 02:57:41 +0530

    Update views.py

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 
 def index(request):
@@ -10,4 +10,15 @@ def add_task(request):
         title = request.POST.get("title")
         if title:
             Task.objects.create(title=title)
+    return redirect("index")
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect("index")
+
+def toggle_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.completed = not task.completed
+    task.save()
     return redirect("index")
